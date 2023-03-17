f4813fcc63f16b31652fbd29a6c2f62882f5e573
Per Guth <mail@perguth.de>
2023-03-17 23:08:01 +0100

    Text justified and pill buttons on index page

--- i18n/index-html.js
+++ i18n/index-html.js
@@ -1,15 +1,17 @@
 const translation = {
   'about-1': 'Welcome to Thingylabs! We are <b>your reliable partner for individual software solutions</b>. As experts in modern web technologies, we specialize in implementing your visions and achieving your digital goals.',
   'about-2': 'As <b>your equal partner</b>, we develop tailor-made solutions and work closely with you. We support companies of all sizes on their way to digital transformation.',
-  'about-3': '<a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" href="https://calendar.app.google/Ejd527BtwpA4TDyr7">Book an appointment</a>',
+  'about-3': '<a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" href="https://calendar.app.google/Ejd527BtwpA4TDyr7">Book an appointment!</a>',
   'case-study-1': 'Case study',
   'case-study-2': 'PANTARIS (formerly Calponia)',
   'case-study-3': 'Find out how we support the Bosch Group on their cloud platform <a referrerpolicy="no-referrer" class="link underline-hover" href="https://www.etas.com/en/applications/pantaris.php">PANTARIS</a> from the technical planning to the international productive operation and continue to actively support them. <span class="i dark-grey">...more on that shortly!</span>',
   'clients': 'Clients',
   'founder': 'Founder',
   'referral-1': 'Referral program',
-  'referral-2': 'Earn as a project broker with Thingylabs:<br>10% commission of each invoice up to a cap of 10,000€.<br><a class="link underline-hover" href="referral.html#/en">Learn more!</a>',
-  'carrer-1': 'Career',
-  'career-2': 'We are looking for IT professionals with broad knowledge of cloud and web technologies.<br><a class="link underline-hover" href="career.html">Learn more!</a>',
+  'referral-2': 'Earn as a project broker with Thingylabs: 10% commission of each invoice up to 10,000€ per client.',
+  'referral-3': '<a class="f6 grow no-underline br-pill ba ph3 pv2 mb2 dib dark-blue" href="referral.html#/en">Learn more!</a>',
+  'career-1': 'Career',
+  'career-2': 'We are looking for IT professionals with broad knowledge of cloud and web technologies.',
+  'career-3': '<a class="f6 grow no-underline br-pill ba ph3 pv2 mb2 dib dark-blue" href="career.html">Learn more!</a>',
   'footer': 'Founded 2017'
 }
